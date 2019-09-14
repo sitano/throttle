4d2f9ff9f3d9dbf9cbc63bc5e677fcf14f7bf627
John.Koepi <john.koepi@gmail.com>
2019-09-14 12:43:36 +0200

    more utilization stat for incoming conns, more consistent write preassure for long int test

--- long_test.go
+++ long_test.go
@@ -6,7 +6,6 @@ package throttle
 import (
 	"context"
 	"fmt"
-	"math/rand"
 	"net"
 	"strconv"
 	"sync/atomic"
@@ -16,8 +15,8 @@ import (
 
 // TestHour runs a server consumer test for 1 hour.
 // It uses 1MB/s total server throughput with unlimited channels.
+//
 // Unlimited server conns allows consuming of the whole throughput by 1 conn.
-// Writers are unlimited but randomly picks write bursts.
 //
 // This test shows if the throttler is unfair on unlimited throttle
 // leafs but limited root bucket. If the consume sizes are small
@@ -29,10 +28,11 @@ func TestHour(t *testing.T) {
 	const threads = 10
 
 	ts := NewTestSystem(t)
-	overallRead := NewMeasureBandwidth(t, bandwidth, "overall_read")
+	overallRead := NewMeasureBandwidth(t, bandwidth, "total_read ")
+	overallWrite := NewMeasureBandwidth(t, bandwidth, "total_wrote")
 	var ag15stat [threads]uint64
 	ts.StartListener(func(ctx context.Context, id uint64, conn net.Conn) {
-		var buf = make([]byte, bandwidth)
+		var buf = make([]byte, bandwidth/10)
 		var stat = NewMeasureBandwidth(t, bandwidth, "read")
 
 		go func() {
@@ -51,13 +51,18 @@ func TestHour(t *testing.T) {
 				for i := 0; i < len(w15stat); i++ {
 					w15sum += w15stat[i]
 				}
+				ag15sum := uint64(0)
+				for i := 0; i < len(ag15stat); i++ {
+					ag15sum += atomic.LoadUint64(&ag15stat[i])
+				}
 
 				fmt.Println(
 					">>>",
 					m.op,
 					"id=", id,
 					"total=", m.Consumed(),
-					"u=", fmt.Sprintf("%0.4f", float64(m.Consumed()-prevRead)/bandwidth),
+					"tu=", fmt.Sprintf("%0.3f", float64(m.Consumed())/float64(ag15sum)),
+					"du=", fmt.Sprintf("%0.3f", float64(m.Consumed()-prevRead)/bandwidth),
 					"ds=", m.Consumed()-prevRead,
 					"dsa/15s=", w15sum/15,
 					"dss/15s=", w15sum,
@@ -78,7 +83,7 @@ func TestHour(t *testing.T) {
 			}
 			stat.Consume(uint64(n))
 			overallRead.Consume(uint64(n))
-			fmt.Println("id=", id, "consumed", stat.Consumed(), "last", n, "since", time.Since(stat.s))
+			//fmt.Println("id=", id, "consumed", stat.Consumed(), "last", n, "since", time.Since(stat.s))
 			if ctx.Err() != nil {
 				break
 			}
@@ -113,7 +118,7 @@ func TestHour(t *testing.T) {
 		var stat = NewMeasureBandwidth(t, bandwidth, "write")
 
 		for {
-			send := buf[:1+rand.Intn(10*bandwidth)]
+			send := buf[:bandwidth]
 			fmt.Println("id=", id, "writing", len(send), "since", time.Since(stat.s))
 			n, err := conn.Write(send)
 			if err != nil {
@@ -126,6 +131,7 @@ func TestHour(t *testing.T) {
 				t.Error("id=", id, "invalid len:", n, "!=", len(buf))
 			}
 			stat.Consume(uint64(n))
+			overallWrite.Consume(uint64(n))
 			fmt.Println("id=", id, "wrote", stat.Consumed(), "last", n, "since", time.Since(stat.s))
 			if ctx.Err() != nil {
 				break
@@ -149,36 +155,57 @@ func TestHour(t *testing.T) {
 			"total =", stat.Consumed(),
 			"max = ", stat.Projected(time.Since(stat.s)),
 			"in =", time.Since(stat.s))
-	}, 0, threads)
+	}, bandwidth, threads)
 
 	start := time.Now()
-	var lastConsumed uint64
+	var lastTotalRead uint64
+	var lastTotalWrite uint64
 	for time.Since(start) < window {
 		time.Sleep(time.Second)
 
-		m := overallRead
-		dt := time.Since(m.s)
-		projected := m.Projected(dt)
-		accuracy := m.Accuracy(projected)
+		{
+			m := overallRead
+			dt := time.Since(m.s)
+			projected := m.Projected(dt)
+			accuracy := m.Accuracy(projected)
 
-		ag15sum := uint64(0)
-		for i := 0; i < len(ag15stat); i++ {
-			ag15sum += atomic.LoadUint64(&ag15stat[i])
+			ag15sum := uint64(0)
+			for i := 0; i < len(ag15stat); i++ {
+				ag15sum += atomic.LoadUint64(&ag15stat[i])
+			}
+
+			fmt.Println(
+				">>>",
+				m.op,
+				"total=", m.Consumed(),
+				"max=", projected,
+				"acc=", fmt.Sprintf("%.3f", accuracy),
+				"ds=", m.Consumed()-lastTotalRead,
+				"avg/15s=", ag15sum/15,
+				"sum/15s=", ag15sum,
+				"util/15s=", fmt.Sprintf("%.3f", float64(ag15sum)/float64(bandwidth*15)),
+				"in=", dt)
+
+			lastTotalRead = m.Consumed()
+		}
+		{
+			m := overallWrite
+			dt := time.Since(m.s)
+			projected := m.Projected(dt)
+			accuracy := m.Accuracy(projected)
+
+			fmt.Println(
+				">>>",
+				m.op,
+				"total=", m.Consumed(),
+				"max=", projected,
+				"acc=", fmt.Sprintf("%.3f", accuracy),
+				"ds=", m.Consumed()-lastTotalWrite,
+				"in=", dt)
+
+			lastTotalWrite = m.Consumed()
 		}
 
-		fmt.Println(
-			">>>",
-			m.op,
-			"total=", m.Consumed(),
-			"proj=", projected,
-			"acc=", accuracy,
-			"ds=", m.Consumed()-lastConsumed,
-			"avg/15s=", ag15sum/15,
-			"sum/15s=", ag15sum,
-			"util/15s=", float64(ag15sum)/float64(bandwidth*15),
-			"in=", dt)
-
-		lastConsumed = m.Consumed()
 	}
 
 	ts.Stop()
